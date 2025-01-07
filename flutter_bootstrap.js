7139a9b5ea4ba8828fdf59c15f7cce92d69b89f7
Sheyla Aulya <Sheylaulyaa@gmail.com>
2025-01-07 16:51:37 +0700

    Deploy SIT Batch 3 Part 6

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "673263062"
+    serviceWorkerVersion: "1955936456"
   }
 });
