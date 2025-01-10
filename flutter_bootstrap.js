89af91731d87c9f9098bd4a20fa555ba8dcd4fb9
Sheyla Aulya <Sheylaulyaa@gmail.com>
2025-01-10 12:05:08 +0700

    Deploy Sort By

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1445817703"
+    serviceWorkerVersion: "3928332860"
   }
 });
